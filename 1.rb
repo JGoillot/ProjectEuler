d0339c033bac9bf00716c39b2157c19767e67fed
JFachetti <goillot.jeremy@gmail.com>
2017-03-05 19:55:14 +0100

    01 - :white_check_mark:

---
+++ 1.rb
@@ -0,0 +1,13 @@
+def sum_of_three_and_five(num)
+  sum = 0
+  num.times do |i|
+    if i % 3 == 0 or i % 5 == 0
+      sum += i
+    end
+  end
+  "Result: #{sum}"
+end
+
+puts sum_of_three_and_five(10)
+puts sum_of_three_and_five(1000)
+
