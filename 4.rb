c8ba0f7d2660b910375bc53ed8abf34e3ba0a431
JFachetti <goillot.jeremy@gmail.com>
2017-03-12 16:50:10 +0100

    04 - :white_check_mark:

---
+++ 4.rb
@@ -0,0 +1,14 @@
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+max = 0
+
+(100..999).each do |a|
+  (a..999).each do |b|
+    product = a * b
+    s_s = product.to_s
+    if product > max and s_s == s_s.reverse
+      max = product
+    end
+  end
+end
+
+puts max
