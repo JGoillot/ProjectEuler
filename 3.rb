d40e9af2de4d94164d996a1ccd860cbd7f28251b
JFachetti <goillot.jeremy@gmail.com>
2017-03-12 11:23:11 +0100

    03 - :white_check_mark:

---
+++ 3.rb
@@ -0,0 +1,24 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# https://en.wikipedia.org/wiki/Prime_factor
+# n = 600851475143
+n = 864
+
+def factor(n)
+  factors = []
+  m = 2
+
+  loop do
+    break if n == 1
+
+    if n % m == 0
+      factors << m
+      n =  n / m
+    else
+      m += 1
+    end
+  end
+
+  return factors
+end
+
+puts factor(n)
