e422ba9e7ef48b14920cbb185dcd83a9b637a76a
JFachetti <goillot.jeremy@gmail.com>
2017-03-26 16:13:12 +0200

    04 - :white_check_mark:

--- 4.rb
+++ 4.rb
@@ -1,15 +1,22 @@
 # A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-max = 0
 
-(100..999).each do |a|
-  (a..999).each do |b|
-    product = a * b
-    s_s = product.to_s
-    if product > max and s_s == s_s.reverse
-      puts "With #{a} x #{b}"
-      max = product
+palindrom = []
+
+i = 999
+digit = 999
+
+while i > 1 do
+  while digit > 1 do
+    sum = i * digit
+    r = sum.to_s
+
+    if r == r.reverse
+      palindrom << sum
     end
+    digit -= 1
   end
+   i -= 1
 end
 
-puts max
+puts palindrom.max
+
